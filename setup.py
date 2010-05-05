69f2cd0b07614dc8613b0a25dd56a223493491f1
Tres Seaver <tseaver@palladion.com>
2010-05-05 17:38:12 +0000

    Conform to repository policy.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 ##############################################################################
 #
-# Copyright (c) 2006 Zope Corporation and Contributors.
+# Copyright (c) 2006 Zope Foundation and Contributors.
 # All Rights Reserved.
 #
 # This software is subject to the provisions of the Zope Public License,
@@ -28,7 +28,7 @@ def read(*rnames):
 
 setup(name = 'zope.catalog',
       version = '3.8.2dev',
-      author='Zope Corporation and Contributors',
+      author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       description='Cataloging and Indexing Framework for the Zope Toolkit',
       long_description=(
