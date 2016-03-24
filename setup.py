272f810bab6c4611fe0367135f723098fae0be13
Tres Seaver <tseaver@palladion.com>
2016-03-23 22:31:58 -0400

    Drop support for Python 2.6.

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name = 'zope.catalog',
-      version='4.1.1.dev0',
+      version='4.2.0.dev0',
       author='Zope Corporation and Contributors',
       author_email='zope-dev@zope.org',
       description='Cataloging and Indexing Framework for the Zope Toolkit',
@@ -42,7 +42,6 @@ setup(name = 'zope.catalog',
           'License :: OSI Approved :: Zope Public License',
           'Programming Language :: Python',
           'Programming Language :: Python :: 2',
-          'Programming Language :: Python :: 2.6',
           'Programming Language :: Python :: 2.7',
           'Programming Language :: Python :: 3',
           'Programming Language :: Python :: 3.3',
