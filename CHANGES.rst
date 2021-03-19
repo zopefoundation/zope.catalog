d483c82a4f7bc9891ab3badadf501f1cd64b471e
Jason Madden <jamadden@gmail.com>
2021-03-19 06:25:55 -0500

    Preparing release 4.3.0

--- CHANGES.rst
+++ CHANGES.rst
@@ -2,7 +2,7 @@
  Changes
 =========
 
-4.3.0 (unreleased)
+4.3.0 (2021-03-19)
 ==================
 
 - Drop support for Python 3.3.
--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ tests_require = [
 ]
 
 setup(name='zope.catalog',
-      version='4.2.2.dev0',
+      version='4.3.0',
       author='Zope Corporation and Contributors',
       author_email='zope-dev@zope.org',
       description='Cataloging and Indexing Framework for the Zope Toolkit',
