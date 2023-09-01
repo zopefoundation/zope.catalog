41d57dc51cb5fa3e067adb3ca391bceed05c6988
Michael Howitz <icemac@gmx.net>
2023-09-01 14:10:25 +0200

    Preparing release 5.0

--- CHANGES.rst
+++ CHANGES.rst
@@ -2,7 +2,7 @@
  Changes
 =========
 
-5.0 (unreleased)
+5.0 (2023-09-01)
 ================
 
 - Drop support for Python 2.7, 3.5, 3.6.
--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ tests_require = [
 ]
 
 setup(name='zope.catalog',
-      version='5.0.dev0',
+      version='5.0',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.dev',
       description='Cataloging and Indexing Framework for the Zope Toolkit',
