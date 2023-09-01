f036d45d54382e3b5ca8491b53286e8f27c746b1
Michael Howitz <icemac@gmx.net>
2023-09-01 12:59:02 +0200

    Bumped version for breaking release.

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ tests_require = [
 ]
 
 setup(name='zope.catalog',
-      version='4.5.dev0',
+      version='5.0.dev0',
       author='Zope Corporation and Contributors',
       author_email='zope-dev@zope.org',
       description='Cataloging and Indexing Framework for the Zope Toolkit',
