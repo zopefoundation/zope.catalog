12d2f3ccd2d54f0e0738a57415515c22e6f367d5
Michael Howitz <icemac@gmx.net>
2024-08-05 08:44:10 +0200

    Configuring for pure-python

--- docs/conf.py
+++ docs/conf.py
@@ -16,6 +16,7 @@
 import sys
 import os
 import pkg_resources
+
 sys.path.append(os.path.abspath('../src'))
 rqmt = pkg_resources.require('zope.catalog')[0]
 
@@ -103,7 +104,6 @@ pygments_style = 'sphinx'
 # If true, keep warnings as "system message" paragraphs in the built documents.
 #keep_warnings = False
 
-
 # -- Options for HTML output ----------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
@@ -188,26 +188,25 @@ html_theme = 'default'
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'zopecatalogdoc'
 
-
 # -- Options for LaTeX output ---------------------------------------------
 
 latex_elements = {
-# The paper size ('letterpaper' or 'a4paper').
-#'papersize': 'letterpaper',
+    # The paper size ('letterpaper' or 'a4paper').
+    #'papersize': 'letterpaper',
 
-# The font size ('10pt', '11pt' or '12pt').
-#'pointsize': '10pt',
+    # The font size ('10pt', '11pt' or '12pt').
+    #'pointsize': '10pt',
 
-# Additional stuff for the LaTeX preamble.
-#'preamble': '',
+    # Additional stuff for the LaTeX preamble.
+    #'preamble': '',
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'zopecatalog.tex', 'zope.catalog Documentation',
-   'Zope Foundation and Contributors', 'manual'),
+    ('index', 'zopecatalog.tex', 'zope.catalog Documentation',
+     'Zope Foundation and Contributors', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -230,19 +229,15 @@ latex_documents = [
 # If false, no module index is generated.
 #latex_domain_indices = True
 
-
 # -- Options for manual page output ---------------------------------------
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
-man_pages = [
-    ('index', 'zopecatalog', 'zope.catalog Documentation',
-     ['Zope Foundation and Contributors'], 1)
-]
+man_pages = [('index', 'zopecatalog', 'zope.catalog Documentation',
+              ['Zope Foundation and Contributors'], 1)]
 
 # If true, show URL addresses after external links.
 #man_show_urls = False
-
 
 # -- Options for Texinfo output -------------------------------------------
 
@@ -250,9 +245,9 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'zopecatalog', 'zope.catalog Documentation',
-   'Zope Foundation and Contributors', 'zopecatalog', 'One line description of project.',
-   'Miscellaneous'),
+    ('index', 'zopecatalog', 'zope.catalog Documentation',
+     'Zope Foundation and Contributors', 'zopecatalog',
+     'One line description of project.', 'Miscellaneous'),
 ]
 
 # Documents to append as an appendix to all manuals.
@@ -267,12 +262,11 @@ texinfo_documents = [
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
 
-
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {
-    'https://docs.python.org/': None,
-    'https://zopecontainer.readthedocs.io/en/latest/': None,
-    'https://zopeindex.readthedocs.io/en/latest/': None,
+    'python': ('https://docs.python.org/', None),
+    'zopecontainer': ('https://zopecontainer.readthedocs.io/en/latest/', None),
+    'zopeindex': ('https://zopeindex.readthedocs.io/en/latest/', None),
 }
 
 autodoc_default_flags = ['members', 'show-inheritance']
