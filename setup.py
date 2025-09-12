e4de754039ccf07b571e2c51e35e1c773581e5e7
Michael Howitz <icemac@gmx.net>
2025-09-12 08:58:39 +0200

    Back to development: 6.1

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ tests_require = [
 ]
 
 setup(name='zope.catalog',
-      version='6.0',
+      version='6.1.dev0',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.dev',
       description='Cataloging and Indexing Framework for the Zope Toolkit',
